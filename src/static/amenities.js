5c4102ebd54588877617349c0a26e2e08a445d57
techteam-web <techteam@brainwing.in>
2026-02-11 15:30:35 +0530

    Changed BG of homepage and then added gsap/react package and created the data file of Aminities

---
+++ src/static/amenities.js
@@ -0,0 +1,34 @@
+const views = [
+  {
+    id: 1,
+    name: "Swimming Pool",
+    src: "/images/amenities/swimming-pool.webp",
+  },
+  {
+    id: 2,
+    name: "Podium",
+    src: "/images/amenities/podium-view.webp",
+  },
+  {
+    id: 3,
+    name: "Pathway",
+    src: "/images/amenities/pathway.webp",
+  },
+  {
+    id: 4,
+    name: "Multipurpose Hall",
+    src: "/images/amenities/multipurpose-hall.webp",
+  },
+  {
+    id: 5,
+    name: "Fitness Center",
+    src: "/images/amenities/fitness-center.webp",
+  },
+  {
+    id: 6,
+    name: "Floor Cut",
+    src: "/images/amenities/floor-cut.webp",
+  },
+];
+
+export default views;
